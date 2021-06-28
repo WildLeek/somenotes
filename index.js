04278bc01306baf941da6ea69a67db5f8213503c
m_goigova <m.goigova@outlook.com>
2021-06-28 06:30:27 +0100

    Rudimentary server running

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-'use strict'
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
 
+//Session storage
+// const redis = require('redis');
+// const redisStore = require('connect-redis')(session);
+// const client = redis.createClient();
+
+const router = express.Router();
+
+const app = express();
+
+//View engine and front end
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+    res.render('main');
+});
+
+app.listen(process.env.PORT||8080,console.log('App running'));
